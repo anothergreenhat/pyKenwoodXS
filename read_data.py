bb333e2a12a5252a8632610f7d5687778175892e
Joseph Madera <jamadera28@gmail.com>
2023-03-09 20:34:23 -0500

    refactored recv() method

--- read_data.py
+++ read_data.py
@@ -2,6 +2,7 @@ import RPi.GPIO as gpio
 from time import sleep
 from time import perf_counter_ns
 from sys import exit
+from xskey import XSKey
 
 SDAT = 24
 CTRL = 23
@@ -25,38 +26,46 @@ def list_to_bin(byte, endian='big'):
         print(f'unknown endian scheme: \'{endian}\'')
         return 0
 
+def recv(): 
+    byte = [None] * 8
+    try:   
+        # start bit
+        gpio.wait_for_edge(SDAT, gpio.RISING)
+        gpio.wait_for_edge(SDAT, gpio.FALLING)
+        # end start bit
+               
+        # begin data bit receive
+        for i in range(8):
+            start = perf_counter_ns()
+            gpio.wait_for_edge(SDAT, gpio.RISING)
+            end = perf_counter_ns()
+            byte[i] = 1 if ((end - start) < 9e6) else 0
+            gpio.wait_for_edge(SDAT, gpio.FALLING)
+                
+        # ctrl goes low which means tranmission has ended
+        gpio.wait_for_edge(CTRL, gpio.FALLING)
+        B = list_to_bin(byte)
+        val = XSKey(B)
+        print(f"{val.name:<20}", end=' ')
+    except KeyboardInterrupt:
+        print()
+        return False
+    except ValueError:
+        print("read_data.py: not a predefined key in XSKey():", end=' ')
+
+    print(f"{bin(B):>12} or {hex(B)} or {B}")
+    return True
+
 def main():
     setup()
     print('To end execution, press Ctrl+C (^C) and then a key on the remote')
     print('Press a key on the Kenwood RC-5030 remote control...')
-    try:
-        while True:
-            byte = [None] * 8
-            
-            # start bit
-            gpio.wait_for_edge(SDAT, gpio.RISING)
-            gpio.wait_for_edge(SDAT, gpio.FALLING)
-            # end start bit
-            
-            
-            # begin data bit receive
-            for i in range(8):
-                start = perf_counter_ns()
-                gpio.wait_for_edge(SDAT, gpio.RISING)
-                end = perf_counter_ns()
-                byte[i] = 1 if ((end - start) < 9e6) else 0
-                gpio.wait_for_edge(SDAT, gpio.FALLING)
-                
-            # ctrl goes low which means tranmission has ended
-            gpio.wait_for_edge(CTRL, gpio.FALLING)
-            B = list_to_bin(byte)
-            print(bin(B), 'or', hex(B), 'or', B)
-    except KeyboardInterrupt:
-        print()
-        exit(0)
-    finally:
-        gpio.cleanup()
-   
-   
-    
-main()
+    status = True
+    while status is True:
+        status = recv()
+
+    gpio.cleanup() 
+
+
+if __name__ == '__main__': 
+    main()
